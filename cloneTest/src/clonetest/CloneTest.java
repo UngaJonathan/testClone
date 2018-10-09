c7a9286e2b331a1f323128463f6ad23bf428465a
Jonathan Unga <38771415+jonunga@users.noreply.github.com>
2018-10-09 11:22:11 -0700

    10/09/18 - clone and push test

---
+++ cloneTest/src/clonetest/CloneTest.java
@@ -0,0 +1,20 @@
+/*
+ * This is a test for cloning a repository and pushing it to the remote
+ * repository location
+ */
+package clonetest;
+
+/**
+ *
+ * @author Jonathan
+ */
+public class CloneTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
